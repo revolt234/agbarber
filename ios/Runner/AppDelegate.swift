9e4057184cc187bcb69d43280dd9d3a708780c73
revolt234 <lorscorz@gmail.com>
2026-07-14 15:41:51 +0200

    migliorato calendario barbiere

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,7 +14,31 @@ import flutter_local_notifications // AGGIUNTO: Importa il plugin per le notific
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
 
+    // AGGIUNTO: Azzera il contatore del badge all'avvio dell'applicazione
+    if #available(iOS 16.0, *) {
+      UNUserNotificationCenter.current().setBadgeCount(0) { error in
+        if let error = error {
+          print("Errore azzeramento badge avvio: \(error.localizedDescription)")
+        }
+      }
+    } else {
+      application.applicationIconBadgeNumber = 0
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // AGGIUNTO: Intercetta l'istante in cui l'app torna in primo piano/attiva per azzerare definitivamente il badge
+  override func applicationDidBecomeActive(_ application: UIApplication) {
+    if #available(iOS 16.0, *) {
+      UNUserNotificationCenter.current().setBadgeCount(0) { error in
+        if let error = error {
+          print("Errore azzeramento badge active: \(error.localizedDescription)")
+        }
+      }
+    } else {
+      application.applicationIconBadgeNumber = 0
+    }
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
